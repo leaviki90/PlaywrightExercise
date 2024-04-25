970ab2c88a8b4c72d60083f657be770d08767531
leaviki90 <leaviki90@gmail.com>
2024-04-25 22:35:47 +0200

    Locators & Annotations

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,9 @@ test('To have title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
+  const getBtn = page.locator(".getStarted_Sjon");
+  await expect(getBtn).toHaveText("Get started");
 });
 
 /*test('get started link', async ({ page }) => {
@@ -18,3 +21,23 @@ test('To have title', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });*/
+
+test("Simple click test", async ({page, browserName}) => {
+  test.skip(browserName === 'firefox', "Working on the firefox fix");
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator("text=Add/Remove Elements").click();
+  //await page.pause();
+  await page.locator("text=Add Element").click();
+ 
+  
+})
+
+test("Duplicate test", async ({page}) => {
+
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.click("text=Add/Remove Elements"); //second way of writing
+  //await page.pause();
+  await page.click("text=Add Element"); //third way would be in a constant
+ 
+  
+})
