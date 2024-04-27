0afec49535c2adb7f9f2ddf0628d9b994c9f7bd8
leaviki90 <leaviki90@gmail.com>
2024-04-27 13:40:20 +0200

    Locators

---
+++ tests/locators.spec.js
@@ -0,0 +1,25 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe("Selectors", () => {
+
+    test("First test", async ({page}) => {
+      await page.goto("https://demoqa.com/text-box");
+        await page.locator("#userName").fill("Lekizmaj"); //type is deprecated
+        // Xpath //input[@placeholder="Full Name"]
+        await page.locator("[placeholder='name@example.com']").fill("lekizmaj@gmail.com");
+        await page.locator("#currentAddress").fill("Generala StatiJaZnam 99");
+        await page.locator("#permanentAddress").fill("Dimitrija Tucovica 99");
+        await page.locator("button:has-text('Submit')").click();
+        //  Some of ways //a[@href="https://demoqa.com"]//img
+        //   //img[@src="/images/Toolsqa.jpg"]
+        // With contains method //span[contains(@class, "rct-title")]
+        // //span[contains(text(), "Home")]
+        //contains() is caseSensitive
+        //Also with XPath we can combine logical operators "or" and "and"
+        // //span[contains(text(), "Home") or @class="rct-title"]
+        // //span[contains(text(), "Home") and @class="rct-title"] 
+   
+
+    })
+})
